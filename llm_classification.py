7f2cb1488f8cb59c25462eeada82578eeee78bea
Samuel Ujazda <samuelujazda@gmail.com>
2025-04-09 12:06:53 +0200

    testing llm_classification

--- llm_classification.py
+++ llm_classification.py
@@ -1,6 +1,26 @@
-import ollama
+from ollama import chat
+from pydantic import BaseModel
 
-def classify_image(img_path):
-    response = ollama.chat(
-        model = "llava:7b",
-        
+class Object(BaseModel):
+    name: str
+    confidance: float
+
+
+path = "cropped_images/1.png"
+objects_path = "objects.json"
+
+response = chat(
+    model="llava:7b",
+    format=Object.model_json_schema(),
+    messages=[
+        {"role": "user",
+         "content": f"Classify the object in the image, choose a object from the provided list, you can only use one object from the list, you can't calssify the object as 'other'. Return as JSON",
+         "images": [path],
+         "objects": [objects_path]
+         }
+    ],
+    options={'temperature' : 0},
+)
+
+image_classification = Object.model_validate_json(response.message.content)
+print(image_classification)
